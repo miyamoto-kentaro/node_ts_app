4394a2b40ce6b02bc5021b27f041cbab738e0b1c
miyamoto-kentaro <kentaro.miyamoto1001@gmail.com>
2021-04-07 12:48:28 +0900

    first-repo

---
+++ src/client/client.ts
@@ -0,0 +1,14 @@
+class Client {
+    private socket: SocketIOClient.Socket
+
+    constructor() {
+        this.socket = io();
+
+        this.socket.on("message", function (message: any) {
+            console.log(message)
+            document.body.innerHTML = message
+        })
+    }    
+}
+
+const client = new Client();
